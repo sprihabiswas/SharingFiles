5b169ce1720c208a5c36f035b8c0db167ca7f7c0
Spriha Biswas <sprihabiswas@gmail.com>
2016-08-07 15:30:41 +0530

    Requesting a Shared File, Retrieving File Information

--- app/src/main/java/skillbooting/sharingfiles/MainActivity.java
+++ app/src/main/java/skillbooting/sharingfiles/MainActivity.java
@@ -1,13 +1,108 @@
 package skillbooting.sharingfiles;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent mRequestFileIntent;
+    private ParcelFileDescriptor mInputPFD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRequestFileIntent = new Intent(Intent.ACTION_PICK);
+        mRequestFileIntent.setType("image/jpg");
+    }
+
+    protected void requestFile(View v) {
+        /**
+         * When the user requests a file, send an Intent to the
+         * server app.
+         * files.
+         */
+        startActivityForResult(mRequestFileIntent, 0);
+    }
+
+    /*
+     * When the Activity of the app that hosts files sets a result and calls
+     * finish(), this method is invoked. The returned Intent contains the
+     * content URI of a selected file. The result code indicates if the
+     * selection worked or not.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent returnIntent) {
+
+        // If the selection didn't work
+        if (resultCode != RESULT_OK) {
+            // Exit without doing anything else
+            return;
+        } else {
+
+                    /*
+             * Get the file's content URI from the incoming Intent,
+             * then query the server app to get the file's display name
+             * and size.
+             */
+            Uri returnUri = returnIntent.getData();
+
+            //get image from uri and display
+            /**ImageView iv = (ImageView)findViewById(R.id.iv_file);
+            iv.setImageURI(returnUri);*/
+            Cursor returnCursor =
+                    getContentResolver().query(returnUri, null, null, null, null);
+            /*
+             * Get the column indexes of the data in the Cursor,
+             * move to the first row in the Cursor, get the data,
+             * and display it.
+             */
+            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+            int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+            returnCursor.moveToFirst();
+            TextView nameView = (TextView) findViewById(R.id.tv_file_name);
+            TextView sizeView = (TextView) findViewById(R.id.tv_file_size);
+            nameView.setText(returnCursor.getString(nameIndex));
+            sizeView.setText(Long.toString(returnCursor.getLong(sizeIndex)));
+
+            returnCursor.close();
+
+            /*
+             * Try to open the file for "read" access using the
+             * returned URI. If the file isn't found, write to the
+             * error log and return.
+             */
+            try {
+                /*
+                 * Get the content resolver instance for this context, and use it
+                 * to get a ParcelFileDescriptor for the file.
+                 */
+                mInputPFD = getContentResolver().openFileDescriptor(returnUri, "r");
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Log.e("MainActivity", "File not found.");
+                return;
+            }
+            // Get a regular file descriptor for the file
+            FileDescriptor fd = mInputPFD.getFileDescriptor();
+
+        }
     }
 }
